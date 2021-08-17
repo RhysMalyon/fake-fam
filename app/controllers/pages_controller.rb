04a0da7f3212a7bb0a8d44909116ae9f0b4d4697
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-17 16:35:24 +0900

    added custom container to cards on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @roles = ["mum", "dad", "girlfriend", "boyfriend", "sister", "brother", "aunty", "uncle", "son", "daughter", "niece", "nephew", "grandpa", "grandma"]
+    @roles = ["mum", "dad", "girlfriend", "boyfriend", "sister", "brother", "aunty", "uncle", "son", "daughter", "grandpa", "grandma"]
   end
 end
