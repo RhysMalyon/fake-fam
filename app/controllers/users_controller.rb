c0b1e56f5f19d09ccf0a6358ebe58228638ceefd
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-17 10:44:10 +0900

    installed pundit and added private methods to user controller to be used in actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :age, :location)
+  end
+
+  def authorize_user
+    authorize @user
+  end
 end
