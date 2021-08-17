0e3df2285afe0d4a6775c57d98064107daecdb2e
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-17 10:37:28 +0900

    added user policy

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,13 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  private
+
+  def user_is_owner?
+    user == record.user
+  end
+end
