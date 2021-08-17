038c39e34fbb24a51b9478491deec56266c38146
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-17 10:35:53 +0900

    added private booking_params and authorize_booking methods to use in bookings controller actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,12 @@
 class BookingsController < ApplicationController
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :location, :category)
+  end
+
+  def authorize_booking
+    authorize @booking
+  end
 end
