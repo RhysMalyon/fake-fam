efd52b8b19bb0bf0b7b00916279babd3fdaa88fa
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-17 16:10:08 +0900

    added role images to asset pipeline

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @roles = Role.all
+    @roles = ["mum", "dad", "girlfriend", "boyfriend", "sister", "brother", "aunty", "uncle", "son", "daughter", "niece", "nephew", "grandpa", "grandma"]
   end
 end
