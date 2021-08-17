e1df84ee022be02ba8121548a8d58a2213b2e289
Rhys Malyon <malyonr@gmail.com>
2021-08-17 13:51:44 +0900

    Create roles view

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,2 +1,13 @@
 class RolesController < ApplicationController
+  def show; end
+
+  private
+
+  def role_params
+    params.require(:role).permit(:role, :price, :bio)
+  end
+
+  def set_role
+    @role = Role.find(params[:id])
+  end
 end
