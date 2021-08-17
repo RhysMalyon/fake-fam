b0282114feeb8494f1dc3b49d38448dd518418b2
Seira Yun <discoverdeepseira@gmail.com>
2021-08-17 15:31:24 +0900

    created the view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,7 @@
 class BookingsController < ApplicationController
-
+  def index
+    @bookings = policy_scope(Booking).group_by { |booking| booking.start_time.to_date }
+  end
   private
 
   def booking_params
