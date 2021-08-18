6c480205c60aa15b011425eebfc247cb08ecca01
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-18 12:07:50 +0900

    added user photos to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 
 puts "Destroying everything - waaaaaaaaa!"
 Role.destroy_all
@@ -7,15 +8,17 @@ Booking.destroy_all
 puts "Creating users..."
 
 10.times do
-  User.create!(
+  file = URI.open('https://thispersondoesnotexist.com/image')
+  user = User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    password: Faker::Internet.password,
+    email: Faker::Internet.safe_email,
+    password: '123123',
     age: rand(18..90),
     bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, consequuntur. Quisquam, fugiat ducimus maiores reprehenderit nobis doloribus. Vel id sed magni ad accusamus, aliquam numquam quisquam sapiente natus amet soluta.",
     location: Faker::Address.city
   )
+  user.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
 end
 
 puts " Creating roles..."
