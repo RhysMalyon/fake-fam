7817b46dca3a546e615e4f181df4a7b6cbec0188
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-19 12:12:58 +0900

    deleted sister, brother, aunt, uncle, grandpa, grandma from roles on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @roles = ["mum", "dad", "girlfriend", "boyfriend", "sister", "brother", "aunty", "uncle", "son", "daughter", "grandpa", "grandma"]
+    @roles = ["mum", "dad", "girlfriend", "boyfriend", "sister", "brother"]
   end
 end
