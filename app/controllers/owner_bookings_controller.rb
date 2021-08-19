532972c83901db466dc903ca6f2debd9c9b5088b
Seira Yun <discoverdeepseira@gmail.com>
2021-08-19 12:15:09 +0900

    finished the controller

--- app/controllers/owner_bookings_controller.rb
+++ app/controllers/owner_bookings_controller.rb
@@ -1,4 +1,30 @@
 class OwnerBookingsController < ApplicationController
+  before_action :set_booking, only: [:update]
   def index
+    @grouped_bookings = policy_scope(Booking).order(start_time: "ASC").group_by do |booking|
+      booking.start_time.to_date > Date.today ? "upcoming" : "past"
+    end
+  end
+
+  def update
+    if @booking.update(booking_params)
+      redirect_to owner_bookings_path(@booking)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :location, :category)
+  end
+
+  def authorize_booking
+    authorize @booking
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 end
