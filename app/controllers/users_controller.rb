57dba4d7edea4d4ffc0abfe49f5ff25db15ba2f4
Rhys Malyon <malyonr@gmail.com>
2021-08-19 17:27:27 +0900

    Create user profile and link to role booking form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit]
+  
+  def show
+    authorize @user
+  end
 
   private
 
@@ -8,5 +13,9 @@ class UsersController < ApplicationController
 
   def authorize_user
     authorize @user
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
