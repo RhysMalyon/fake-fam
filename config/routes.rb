e275895627d1730b8adeb8e6fadd42f41f6692a6
Seira Yun <discoverdeepseira@gmail.com>
2021-08-19 11:39:16 +0900

    fixed the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'owner_bookings/index'
   devise_for :users
   root to: 'pages#home'
-  
+
   resources :roles, only: %i[index show new create] do
     resources :bookings, only: %i[new create]
   end
