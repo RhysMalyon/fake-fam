87e87dd683e32ce10912c96c52387ed54e5016f1
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-08-20 16:07:57 +0900

    fixed review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_031134) do
+ActiveRecord::Schema.define(version: 2021_08_20_063418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,9 @@ ActiveRecord::Schema.define(version: 2021_08_20_031134) do
     t.bigint "role_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["role_id"], name: "index_reviews_on_role_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
@@ -90,5 +92,6 @@ ActiveRecord::Schema.define(version: 2021_08_20_031134) do
   add_foreign_key "bookings", "roles"
   add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "roles"
+  add_foreign_key "reviews", "users"
   add_foreign_key "roles", "users"
 end
